035b7a3125ab924cbefb5bb32b3e90fb4620d27e
Kostiantyn Kolontai <kkolontay@gmail.com>
2024-06-03 15:30:10 -0400

    Update InfoView.kt

--- app/src/main/java/com/kkolontay/theworld/ui/screens/InfoView.kt
+++ app/src/main/java/com/kkolontay/theworld/ui/screens/InfoView.kt
@@ -1,4 +1,6 @@
 package com.kkolontay.theworld.ui.screens
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -7,15 +9,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun InfoView(modifier: Modifier = Modifier.fillMaxWidth().padding(16.dp), taps: Int, refresh: () -> Unit, back: Int) {
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        Text("Taps: ")
+fun InfoView(modifier: Modifier = Modifier
+    .fillMaxWidth()
+    .padding(16.dp), taps: Int, refresh: () -> Unit, back: Int) {
+    Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceBetween ) {
+        Row {
+            Text("Taps: ")
+            Text("$taps", modifier = Modifier.padding(start = 8.dp))
+
+        }
         Button(onClick = { refresh() }) {
             Text(text = "Refresh")
         }
-        Text("Back: ")
+        Row {
+            Text("Back: ")
+            Text("$back", modifier = Modifier.padding(start = 8.dp))
+
+        }
     }
 }
