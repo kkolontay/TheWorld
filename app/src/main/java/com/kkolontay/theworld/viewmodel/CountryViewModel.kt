53c850020018a4147e5fbbf92af4e2566ce5d1be
Kostiantyn Kolontai <kkolontay@gmail.com>
2024-06-10 23:52:05 -0400

    final implementation

--- app/src/main/java/com/kkolontay/theworld/viewmodel/CountryViewModel.kt
+++ app/src/main/java/com/kkolontay/theworld/viewmodel/CountryViewModel.kt
@@ -3,19 +3,12 @@ package com.kkolontay.theworld.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.kkolontay.theworld.api.CountryInfoState
 import com.kkolontay.theworld.flow.Flows
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class CountryViewModel : ViewModel() {
-    private val _uiState: MutableStateFlow<CountryInfoState> = MutableStateFlow(CountryInfoState.Loading())
-    val uiState = _uiState.asStateFlow()
-   // private val apiService = ApiBuilder.apiService
    private val _timerState: MutableStateFlow<Int> = MutableStateFlow(0)
     val timerState = _timerState.asStateFlow()
     private val _tap: MutableStateFlow<Int> = MutableStateFlow(0)
@@ -25,15 +18,7 @@ class CountryViewModel : ViewModel() {
     val flows = Flows
 
     init {
-//        viewModelScope.launch {
-//            getCountryInfoFlow().catch {
-//                _uiState.value = CountryInfoState.Error(it.localizedMessage)
-//            }
-//                .collect {
-//                    _uiState.value = it
-//                    flows.counter = 1_000_000
-//                }
-//        }
+
         viewModelScope.launch {
 
             flows.fetchBackFlow().collect {
@@ -55,11 +40,9 @@ class CountryViewModel : ViewModel() {
                 println(it)
             }
         }
-
     }
 
     fun refresh() {
-        _uiState.value = CountryInfoState.Loading()
         flows.counter = 0
         viewModelScope.launch {
             flows.timer.collect {
@@ -67,25 +50,5 @@ class CountryViewModel : ViewModel() {
                 println(it)
             }
         }
-        viewModelScope.launch {
-//            getCountryInfoFlow().catch {
-//                _uiState.value = CountryInfoState.Error(it.localizedMessage)
-//            }
-//                .collect {
-//                    _uiState.value = it
-//                    flows.counter = 1_000_000
-//                }
-        }
     }
-
-//    private fun getCountryInfoFlow(): Flow<CountryInfoState> {
-//      return  flow {
-//            val response = apiService.getAllCountry()
-//          if (response.isSuccessful) {
-//              emit(CountryInfoState.Success(response.body() ?: listOf()))
-//          } else {
-//              emit(CountryInfoState.Error(response.errorBody().toString()))
-//          }
-//        }
-//    }
 }
